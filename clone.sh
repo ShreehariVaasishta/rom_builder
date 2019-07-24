d942906f5128fbd82a8d8c5f117ea00c6003eec3
ShreehariVaasishta <shreehari9481@gmail.com>
2019-07-24 20:55:16 +0530

    fix branch name

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # write commands if you want to clone some repo
-git clone https://GARYHARY@bitbucket.org/GARYHARY/device_smartron_rimo02a.git -b  dot-p device/smartron/rimo02a
+git clone https://GARYHARY@bitbucket.org/GARYHARY/device_smartron_rimo02a.git -b  dotp device/smartron/rimo02a
 git clone https://GARYHARY@bitbucket.org/GARYHARY/vendor_smartron_rimo02a.git -b dotp vendor/smartron/rimo02a
 git clone https://GARYHARY@bitbucket.org/GARYHARY/kernel_smartron_msm8976.git -b GoldenEagle kernel/smartron/msm8976
